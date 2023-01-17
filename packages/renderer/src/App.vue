5889fb488de225a705b02c37230d9edcd3579824
zhzhz <932496816@qq.com>
2023-01-17 15:37:15 +0800

    71 vue canvas

--- packages/renderer/src/App.vue
+++ packages/renderer/src/App.vue
@@ -1,54 +1,11 @@
 <script lang="ts" setup>
-import ReactiveCounter from '/@/components/ReactiveCounter.vue';
-import ReactiveHash from '/@/components/ReactiveHash.vue';
-import ElectronVersions from '/@/components/ElectronVersions.vue';
+import MyCanvas from '/@/components/MyCanvas.vue';
 
-const APP_VERSION = import.meta.env.VITE_APP_VERSION;
+//const APP_VERSION = import.meta.env.VITE_APP_VERSION;
 </script>
 
 <template>
-  <img
-    alt="Vue logo"
-    src="../assets/logo.svg"
-    width="150"
-  />
-
-  <p>
-    <!-- Example how to inject current app version to UI -->
-    App version: {{ APP_VERSION }}
-  </p>
-
-  <p>
-    For a guide and recipes on how to configure / customize this project,<br />
-    check out the
-    <a
-      href="https://github.com/cawa-93/vite-electron-builder"
-      target="_blank"
-    >
-      vite-electron-builder documentation
-    </a>
-    .
-  </p>
-
-  <fieldset>
-    <legend>Test Vue Reactivity</legend>
-    <reactive-counter />
-  </fieldset>
-
-  <fieldset>
-    <legend>Test Node.js API</legend>
-    <reactive-hash />
-  </fieldset>
-
-  <fieldset>
-    <legend>Environment</legend>
-    <electron-versions />
-  </fieldset>
-
-  <p>
-    Edit
-    <code>packages/renderer/src/App.vue</code> to test hot module replacement.
-  </p>
+  <MyCanvas />
 </template>
 
 <style>
@@ -60,10 +17,5 @@ const APP_VERSION = import.meta.env.VITE_APP_VERSION;
   color: #2c3e50;
   margin: 60px auto;
   max-width: 700px;
-}
-
-fieldset {
-  margin: 2rem;
-  padding: 1rem;
 }
 </style>
---
+++ packages/renderer/src/components/MyCanvas.vue
@@ -0,0 +1,43 @@
+<script lang="ts" setup>
+import {ref, onMounted} from 'vue';
+
+const canvasRef = ref<HTMLCanvasElement>();
+let ctx:CanvasRenderingContext2D | null = null;
+
+const initContext = () => {
+  if (canvasRef.value == undefined)
+  {
+    return;
+  }
+
+  ctx = canvasRef.value.getContext('2d');
+};
+
+const draw = () => {
+
+  if (ctx == null)
+  {
+    return;
+  }
+  
+  ctx.fillStyle='#FF0000';
+  ctx.fillRect(0,0,70,75);
+};
+
+onMounted(()=>{
+  initContext();
+  draw();
+});
+
+</script>
+
+<template>
+  <canvas
+    ref="canvasRef"
+    style="width: 100%; border-style: solid; border-width: 1px"
+    :width="200"
+    :height="200"
+  >
+    浏览器不支持canvas
+  </canvas>
+</template>

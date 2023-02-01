b735abe7fcc78d0c594afe9cf45562db603f1b88
zhzhz <932496816@qq.com>
2023-02-01 12:14:22 +0800

    ..

--- .nano-staged.mjs
+++ .nano-staged.mjs
@@ -1,14 +1,14 @@
 import {resolve, sep} from 'path';
 
 export default {
-  '*.{js,mjs,cjs,ts,mts,cts,vue}': 'eslint --cache --fix',
+  '*.{mjs,cjs,ts,mts,cts}': 'eslint --cache --fix',
 
   /**
    * Run typechecking if any type-sensitive files or project dependencies was changed
    * @param {string[]} filenames
    * @return {string[]}
    */
-  '{package-lock.json,packages/**/{*.ts,*.vue,tsconfig.json}}': ({filenames}) => {
+  '{package-lock.json,packages/**/{*.ts,tsconfig.json}}': ({filenames}) => {
     // if dependencies was changed run type checking for all packages
     if (filenames.some(f => f.endsWith('package-lock.json'))) {
       return ['npm run typecheck --if-present'];
